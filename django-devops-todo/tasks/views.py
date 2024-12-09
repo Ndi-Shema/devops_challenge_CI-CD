415b8e31161c0796fce1c014ae8ba62b1db94ebc
Ndi-Shema <f.shema@alustudent.com>
2024-12-09 03:27:21 +0200

    redoing devops challenge

---
+++ django-devops-todo/tasks/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the Todo API!")
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
